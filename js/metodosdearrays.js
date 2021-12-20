58ad7fd5f504b8ea6125906c276b1d7e16bcd992
Camilo <camilolaruffa@gmail.com>
2021-12-20 16:47:00 -0300

    Metodos de arrays

--- js/metodosdearrays.js
+++ js/metodosdearrays.js
@@ -1,5 +1,6 @@
-const arrayGod = ["Hola", 10, true, "si", null, {nombre: "Juan", trabajo: "Programador"}, [1,2,3]]
+const arrayGod = ["Hola", 10, true, "si", null, {nombre: "Camilo", trabajo: "Programador"}, [1,2,3]]
 
+//! Es mejor si no los usas 
 arrayGod.pop() // borra el ultimo
 console.log(arrayGod);
 
@@ -12,5 +13,56 @@ console.log(arrayGod);
 arrayGod.splice(3,2) // Borra desde el 3 + 2 espacios
 console.log(arrayGod);
 
-const nuevoArray = [ ...arrayGod ]
+//* Este esta bueno
+const nuevoArray = [ ...arrayGod, "Super nashe" ]
 console.log(nuevoArray);
+
+//* Otros metodos:
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', ]
+const carrito = [
+    { nombre: 'Monitor 20 pulgadas', precio: 500 },
+    { nombre: 'Television 50 pulgadas', precio: 700 },
+    { nombre: 'Tablet', precio: 300 },
+    { nombre: 'Audifonos', precio: 200 },
+    { nombre: 'Teclado', precio: 50 },
+    { nombre: 'Celular', precio: 500 },
+    { nombre: 'Parlante', precio: 300 },
+    { nombre: 'Laptop', precio: 800 },
+];
+
+//! Feo
+meses.forEach(function(mes) {
+    if(mes == 'Marzo') {
+        console.log("Marzo si existe")
+    }
+});
+
+//* Lindo
+let resultado = meses.includes('Febrero')
+console.log(resultado)
+
+//! No funciona con objetos dentro del array
+resultado = carrito.includes('Teclado')
+console.log(resultado);
+
+//* Si tenes objetos tenes que usar Some. Estos 2 metodos sirven para obtener un valor BOOLEAN
+resultado = carrito.some(function(producto) {
+    return producto.nombre === 'Teclado'
+})
+console.log(resultado);
+
+//* Reduce: reduce lo que hace es que "reduce" un array a una variable
+resultado = carrito.reduce(function(total, producto) {
+    return total + producto.precio
+}, 0)
+console.log(resultado);
+
+//* Filter
+resultado = carrito.filter(function(producto) {
+    return producto.precio > 400
+})
+
+resultado = carrito.filter(function(producto) {
+    return producto.nombre != 'Teclado'
+})
+console.log(resultado);
