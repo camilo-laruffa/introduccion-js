36db9d3ab820e0094d9ea15e185936f1f68e7097
Camilo <camilolaruffa@gmail.com>
2021-12-21 12:30:01 -0300

    Notification API

---
+++ js/notificationAPI.js
@@ -0,0 +1,14 @@
+//* Seleccionamos el boton desde su ID
+const boton = document.querySelector('#boton');
+boton.addEventListener('click', () => {
+    // La API es un promise
+    Notification.requestPermission()
+        .then(resultado => console.log(`El resultado es ${resultado}`))
+});
+
+if(Notification.permission == 'granted') {
+    new Notification('Esta es una notificacion', {
+        icon: 'img/ccj.png',
+        body: 'Los tutoriales de juan'
+    })
+}
